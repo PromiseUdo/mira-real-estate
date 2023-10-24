ea8c0c6b742bf975fd1c749a74b7e17a7d4dec43
PromiseUdo <info.promiseudo@gmail.com>
2023-10-24 15:31:01 +0100

    mobile responsiveness for newsletter signup1

--- src/app/components/NewsletterSignup.jsx
+++ src/app/components/NewsletterSignup.jsx
@@ -15,7 +15,7 @@ const NewsletterSignup = () => {
           <p className="text-center md:text-start">
             Enter your email to get latest information on new listings{" "}
           </p>
-          <div className="flex items-center gap-2">
+          <div className="flex flex-col md:flex-row items-center gap-2">
             <div className="w-full flex items-center  border py-2 px-4 my-2 rounded-lg">
               <input
                 className=" text-white w-full bg-transparent outline-none"
