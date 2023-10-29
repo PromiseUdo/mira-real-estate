03ba3d6807d4f00d87d47f616ce9550462faf023
PromiseUdo <info.promiseudo@gmail.com>
2023-10-29 15:42:36 +0100

    update

--- src/app/(routes)/properties/for_sale/page.js
+++ src/app/(routes)/properties/for_sale/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Filter from "./components/Filter";
-import TypeFilter from "../components/TypeFilter";
 import DummyContent from "../../../components/DummyContent";
+import { BsPinMapFill } from "react-icons/bs";
 import FilterResults from "../for_sale/components/FilterResults.jsx";
 const page = () => {
   return (
@@ -15,6 +15,12 @@ const page = () => {
         <div className="shadow	shadow-zee-gray-600	bg-gray-50  flex-1 mr-0 lg:mr-4  overflow-y-scroll   scrollbar    scrollbar-w-1.5    scrollbar-thumb-zee-dark-teal-80 scrollbar-track-zee-gray-200 ">
           <FilterResults />
         </div>
+      </div>
+      <div className="w-full flex items-center justify-center fixed bottom-5">
+        <button className="bg-zee-teal-20 py-2 px-4 rounded-md flex gap-2 items-center justify-center">
+          <BsPinMapFill />
+          <span>Map</span>
+        </button>
       </div>
     </div>
   );
