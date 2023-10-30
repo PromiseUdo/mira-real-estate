c3f42a01cce51798c3ce8e8018cde58cb47860f5
PromiseUdo <info.promiseudo@gmail.com>
2023-10-30 15:57:58 +0100

    property detail overlay

--- src/app/(routes)/properties/components/PropertyDetail.jsx
+++ src/app/(routes)/properties/components/PropertyDetail.jsx
@@ -2,36 +2,39 @@ import Link from "next/link";
 import React from "react";
 import { BiShareAlt } from "react-icons/bi";
 import { MdReportGmailerrorred, MdKeyboardArrowLeft } from "react-icons/md";
+import DummyContent from "../../../components/DummyContent";
 
 const PropertyDetail = ({ onClose }) => {
   return (
     <div
-      style={{
-        overflow: "hidden !important",
-      }}
-      className="!overflow-hidden	 !overscroll-none h-screen w-full bg-white grid grid-cols-1
-     md:grid-cols-12"
+      // style={{
+      //   overflow: "hidden !important",
+      // }}
+      className="h-screen w-full bg-white grid grid-cols-1
+     md:grid-cols-12
+     overflow-y-scroll   scrollbar    scrollbar-w-1.5    scrollbar-thumb-zee-dark-teal-80 scrollbar-track-zee-gray-200
+     "
     >
-      <div className=" h-64  !overscroll-none md:h-screen col-span-7  bg-gray-100 ">
-        <div className="fixed w-full top-0 bg-green-200 flex  py-2 md:hidden justify-between items-center px-4">
-          <button className="logo">
-            <MdKeyboardArrowLeft onClick={onClose} size={20} />
+      <div className="sticky  w-full top-0 bg-green-200 flex  py-2 md:hidden justify-between items-center px-4">
+        <button className="logo">
+          <MdKeyboardArrowLeft onClick={onClose} size={20} />
+        </button>
+
+        <div className="options flex items-center gap-4">
+          <button className="text-zee-teal-100 capitalize flex items-center gap-1 font-normal">
+            <BiShareAlt color="#00a0ae" size={20} />
           </button>
 
-          <div className="options flex items-center gap-4">
-            <button className="text-zee-teal-100 capitalize flex items-center gap-1 font-normal">
-              <BiShareAlt color="#00a0ae" size={20} />
-            </button>
-
-            <button className="text-zee-teal-100 capitalize flex items-center gap-1 font-normal">
-              <MdReportGmailerrorred color="#00a0ae" size={20} />
-            </button>
-          </div>
+          <button className="text-zee-teal-100 capitalize flex items-center gap-1 font-normal">
+            <MdReportGmailerrorred color="#00a0ae" size={20} />
+          </button>
         </div>
+      </div>
+      <div className=" h-64   md:h-screen col-span-7  bg-gray-100 ">
         <span>Pictures of Property</span>
       </div>
 
-      <div className="h-screen col-span-5   bg-red-500  py-1">
+      <div className=" h-screen col-span-5    py-1">
         <div className="hidden border-b pb-1 border-zee-border md:flex justify-between items-center mx-4">
           <div className="logo">
             <Link href="/" className="">
@@ -55,6 +58,8 @@ const PropertyDetail = ({ onClose }) => {
             </button>
           </div>
         </div>
+
+        <span>Property Details</span>
       </div>
     </div>
   );
